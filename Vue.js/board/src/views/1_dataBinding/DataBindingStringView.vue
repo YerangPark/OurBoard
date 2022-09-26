897d235595ed1748259c6be69e300ffaa871a9e4
yerang2zzang <buuuuung@naver.com>
2022-09-26 20:57:07 +0900

    Fix Error: Eslint 문법 오류 수정

---
+++ Vue.js/board/src/views/1_dataBinding/DataBindingStringView.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <h1>Hello {{ userName }}</h1>
+    <p></p>
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userName: 'Yerang Park',
+      message: '',
+      arr: [],
+      obj: {}
+    }
+  }
+}
+</script>
